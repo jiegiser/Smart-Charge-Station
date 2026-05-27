946b41948b1fb99ff902de0c19d37bc4ca9d49f4
jiegiser <changjie@jouav.com>
2026-05-27 13:59:11 +0800

    feat: 初始化项目

---
+++ charge-demo/src/test/java/org/jiegiser/smartchargestation/demo/ChargeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jiegiser.smartchargestation.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChargeDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
